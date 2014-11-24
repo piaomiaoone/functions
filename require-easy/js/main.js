ef40b04bd4e047aa3520b6505e20b2169fd4a976
piaomiaoone <864747537@qq.com>
2014-11-24 12:57:16 +0800

    require ease

---
+++ require-easy/js/main.js
@@ -0,0 +1,3 @@
+/*Copyright 2014 Baidu Inc. All rights reserved.*/
+require.config({shim:{"jquery.scroll":{deps:["jquery"],exports:"jquery.fn.scroll"}}});
+/*last modify: 2014-11-24 12:53:37 */
---
+++ require-easy/js/math.js
@@ -0,0 +1,3 @@
+/*Copyright 2014 Baidu Inc. All rights reserved.*/
+define(function(){var a=function(a,b){return a+b};return{add:a}});
+/*last modify: 2014-11-24 12:53:37 */
